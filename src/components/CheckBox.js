42fef3ba96a2eb2c3927f8bacd9a124742b8bce9
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-18 15:16:37 -0300

    Novos componentes css e atualizações estéticas
    Atualizações estéticas em quase todos componentes

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import styles from './CheckBox.module.css'
 
 export const CheckBox = ({ label }) => {
     return (
         <>
-            <span>{label}</span>
-            <label htmlFor='regular'>Regular:
+            <div className={styles.checkBoxContainer}>
+                <span>{label}</span>
+
+                <label htmlFor='regular'>Regular:</label>
+                <label htmlFor='temporario'>Temporário:</label>
+                <label htmlFor='meio expediente'>Meio expediente:</label>
+                <label htmlFor='estagiário'>Estagiário:</label>
+
+
                 <input type="radio" id='regular' />
-            </label>
-            <label htmlFor='temporario'>Temporário:
                 <input type="radio" id='temporario' />
-            </label>
-            <label htmlFor='meio expediente'>Meio expediente:
                 <input type="radio" id='meio expediente' />
-            </label>
-            <label htmlFor='estagiário'>Estagiário:
                 <input type="radio" id='estagiário' />
-            </label>
+
+            </div>
         </>
     )
 }
--- src/components/Date.js
+++ src/components/Date.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import styles from './Date.module.css'
 
 export const Date = ({ label }) => {
     return (
         <>
-            <label htmlFor='date'>{label}
-                <input type="date" id='date' />
-            </label>
+            <div className={styles.dateContainer}>
+                <label htmlFor='date'>{label}
+                    <input type="date" id='date' />
+                </label>
+            </div>
         </>
     )
 }
