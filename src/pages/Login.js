2ff6619a34fcb8e6cdbe3793128dbd17ed6fe1b9
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-19 13:58:07 -0300

    Atualização da lógica do código em Login.js
    Foi criado a lógica para validar senha e usuário. Também adicionado ao código hook de estado e código para coletar e setar dados.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,37 @@ import React from 'react'
 import styles from './Login.module.css'
 
 export const Login = () => {
+
+    const [usuario, setUsuario] = React.useState('');
+    const [senha, setSenha] = React.useState('');
+
+    const validarUsuario = (usuario) => {
+        return usuario?.toString().includes('@') && usuario?.toString().includes('.')
+
+    }
+
+    const validarSenha = (senha) => {
+        return senha?.toString().length > 6;
+    }
+
+    const handleForm = (event) => {
+        event.preventDefault();
+        console.log(validarUsuario(usuario), validarSenha(senha))
+    }
+
+
     return (
         <div className={styles.loginContainerBg}>
-            <form className={styles.loginContainer}>
+            <form onSubmit={(event) => handleForm(event)} className={styles.loginContainer}>
+                <h1>Faça seu login: </h1>
                 <label htmlFor='usuario'>Usuário</label>
-                <input className={styles.loginInput} type="text" id='usuario' />
+                <input className={styles.loginInput} type="email" id='usuario' placeholder='Digite seu email.' value={usuario} onChange={event => setUsuario(event.target.value)} />
 
                 <label htmlFor='senha'>Senha</label>
-                <input className={styles.loginInput} type="password" id='senha' />
+                <input className={styles.loginInput} type="password" id='senha' placeholder='Digite sua senha.' value={senha} onChange={event => setSenha(event.target.value)} />
 
                 <button className={styles.loginBotao}>Entrar</button>
             </form>
         </div>
     )
-}
+} 
