86cd0e821ee1b22dbc9a376f483dcef705e2dbb9
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-19 09:11:53 -0300

    Novo arquivo Sobre.module.css e página sobre finalizada
    Foi adicionado uma breve descrição do projeto no arquivo Sobre.js

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import styles from './Sobre.module.css'
 
 export const Sobre = () => {
     return (
-        <div>Sobre</div>
+        <div className={styles.sobreContainerBg}>
+            <div className={styles.sobreContainer}>
+                <h4>A página Job.Forms, foi criada com o intuito de praticar JavaScript e React. O projeto, tem o objetivo de criar uma página onde o usuário irá realizar login em sua conta e preencher um formulário. Todos os dados preenchidos serão salvos. </h4>
+            </div>
+        </div >
     )
 }
