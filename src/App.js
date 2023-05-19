ec0f849d2af6236cbd1189c023ad617221888e27
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-19 08:45:04 -0300

    Novos componentes Header.js e Header.module.css
    Adicionando menu e novas páginas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Formulario } from './components/Formulario';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Formulario />} />
       </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Header.module.css'
+import { GrLogin } from "react-icons/gr";
+import { MdWork } from "react-icons/md";
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+    return (
+        <>
+            <header>
+                <nav className={styles.headerMenu}>
+                    <Link className={styles.linkMenuJob} to='/'><MdWork />Job.Forms</Link>
+                    <div className={styles.linkMenuLoginSobre}>
+                        <Link className={styles.linkMenuLogin} to='/formulario'><GrLogin />Login</Link>
+                        <Link className={styles.linkMenuSobre} to='/sobre'>Sobre</Link>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
+}
