825c13105138d6a9b1195198aaddba103f5c8d52
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-19 17:01:22 -0300

    Atualização no código do arquivo Login, criação botão cadastrar
    Foi criado um botão para cadastrar usuários.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,10 +7,12 @@ export const Login = () => {
     const [usuario, setUsuario] = React.useState('');
     const [senha, setSenha] = React.useState('');
     const { setAutenticar, dataBase } = React.useContext(Context);
+    const [usuarioEncontrado, setUsuarioEncontrado] = React.useState(true);
+    const [cadastrar, setCadastrar] = React.useState(false);
+    const [senhaRepetida, setSenhaRepetida] = React.useState('');
 
     const validarUsuario = (usuario) => {
         return usuario?.toString().includes('@') && usuario?.toString().includes('.')
-
     }
 
     const validarSenha = (senha) => {
@@ -21,18 +23,26 @@ export const Login = () => {
         event.preventDefault();
         dataBase.map(element => {
             if (usuario === element.usuario && senha === element.senha) {
-
+                setUsuarioEncontrado(true);
                 setAutenticar(true);
+            } else {
+                setUsuarioEncontrado(false);
             }
         }
         )
+    }
 
-        // console.log(validarUsuario(usuario), validarSenha(senha))
+    const handleClick = () => {
+        setUsuarioEncontrado(true);
+        setCadastrar(true);
+        if (senhaRepetida !== '') {
+            console.log('senha confirmada')
+        }
     }
 
     return (
         <div className={styles.loginContainerBg}>
-            <form onSubmit={(event) => handleForm(event)} className={styles.loginContainer}>
+            <form className={styles.loginContainer}>
                 <h1>Faça seu login: </h1>
                 <label htmlFor='usuario'>Usuário</label>
                 <input className={styles.loginInput} type="email" id='usuario' placeholder='Digite seu email.' value={usuario} onChange={event => setUsuario(event.target.value)} />
@@ -40,7 +50,18 @@ export const Login = () => {
                 <label htmlFor='senha'>Senha</label>
                 <input className={styles.loginInput} type="password" id='senha' placeholder='Digite sua senha.' value={senha} onChange={event => setSenha(event.target.value)} />
 
-                <button className={styles.loginBotao}>Entrar</button>
+                {usuarioEncontrado ? '' : <span style={{ color: 'red' }}>Usuário ou senha errada.</span>}
+
+                {cadastrar ?
+                    <div className={styles.senhaRepetirContainer}>
+                        <label htmlFor='repetirSenha'>Repita sua senha novamente e clique em casdastrar</label>
+                        <input className={styles.loginInput} type="password" id='repetirSenha' placeholder='Digite sua senha novamente.' value={senhaRepetida} onChange={(event) => setSenhaRepetida(event.target.value)} />
+                    </div> : ''}
+
+                <div className={styles.loginBotaoContainer}>
+                    <button type='submit' className={styles.loginBotao} onClick={(event) => handleForm(event)}>Entrar</button>
+                    <button type='button' className={styles.loginBotao} onClick={() => handleClick()}>Cadastrar</button>
+                </div>
             </form>
         </div>
     )
